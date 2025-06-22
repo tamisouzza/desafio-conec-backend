bc0980927650e4728a04d621372964eb1a77fdc7
Tamiris <tamirissoares91@gmail.com>
2025-06-22 18:42:38 -0300

    Corrige conexão com banco para o Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity'; 
+import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -13,14 +13,13 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [User],
       synchronize: true,
-      ssl: {
-        rejectUnauthorized: false,  
-      },
+      entities: [User],
+      autoLoadEntities: true,
     }),
     UsersModule,
     AuthModule,
   ],
 })
 export class AppModule {}
+
